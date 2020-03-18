57eb739f960b8aad7556e1f13124321125565a2b
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-03-18 22:58:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(array)
+  i = 0 
+  result = []
+  while i < array.length do 
+    var = array[i]
+    result << yield(var)
+    i += 1 
+  end 
+  result
+end 
+
+def reduce(array, starting_value = 0)
+  total = starting_value
+  result = 0
+  i = 0 
+  while i < array.length do 
+    var = array[i]
+    result = yield(var, total)
+  end 
+  result 
+end 
